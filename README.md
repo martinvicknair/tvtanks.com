706f199e9903ad27f583b645fac3379bd6f5c803
Martin <martinvicknair@gmail.com>
2023-01-22 13:04:24 -0600

    refactored as a single python script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# tvtanks-scripts
-A repository of scripts used in my Raspberry Pi powered Tv Fish Tank.
+
+# TV Fish Tank Channel Changer
+This project uses a two pole switch connected to the GPIO pins of a Raspberry Pi to deactivate the screensaver and change the background picture of a TV Fish Tank. 
+
+## Resources 
+* [pynput](https://pynput.readthedocs.io/en/latest/index.html)
+* [gpiozero](https://gpiozero.readthedocs.io/en/stable/)
+* [pcmanfm](https://learn.sparkfun.com/tutorials/how-to-run-a-raspberry-pi-program-on-startup/all#method-2-autostart) 
+
+* [TV Tanks](https://tvtanks.com) 
+* [Github Repository](https://github.com/martinvicknair)
--- channel_changer.py
+++ channel_changer.py
@@ -2,35 +2,49 @@
 
 ## Changes the 'channel' on my TV Fish tank:
 ## -Two pole switch connected via GPIO pins of a Raspberry Pi
-##  deactivates the screensaver and changes the background picture
+## -deactivates the screensaver and changes the background picture
 
 import os
 from pynput.mouse import Button, Controller
 from gpiozero import Button
 from signal import pause
+import glob
+import random
+from subprocess import call
 
-mouse = Controller()
-button = Button(2)
+mouse = Controller()  # Initialize mouse controller 
+button = Button(2)    # Initialize button on GPIO pin 2 
 
-# Sets initial mouse position away from center of screen
+# Store directory of wallpapers in variable 
+wp_directory="/home/pi/Desktop/backgrounds/*"
+
+# Sets initial mouse position away from center of screen 
 mouse.position = (0, 50)
 
+# Define action to be taken when button is pressed or released 
+def action():  
 
-def action():
-    os.system("/home/pi/Desktop/random_bg")
-    os.system("xscreensaver-command -deactivate")
-    mouse.position = (0, 50)
+# Select a random wallpaper from the directory 
+    random_file = random.choice(glob.glob(wp_directory))
 
+# Set the wallpaper using pcmanfm  
+    call(["pcmanfm", "--set-wallpaper=" + random_file])  
 
-button.when_pressed = action
-button.when_released = action
+# Deactivate screensaver and move mouse away from center of screen 
+    os.system("xscreensaver-command -deactivate")  
+    mouse.position = (0, 50)  
 
-pause()
+# Set action to be taken when button is pressed or released 
+button.when_pressed = action  
+button.when_released = action  
+
+# Pause program execution until button is pressed or released 
+pause()  
 
 # https://pynput.readthedocs.io/en/latest/index.html
 # https://gpiozero.readthedocs.io/en/stable/
 # https://gpiozero.readthedocs.io/en/stable/recipes.html#button
 # https://learn.sparkfun.com/tutorials/how-to-run-a-raspberry-pi-program-on-startup/all#method-2-autostart
-#
+
 ## https://tvtanks.com
 ## https://github.com/martinvicknair
