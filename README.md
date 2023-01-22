7f428123240ff337294a088d1a55e22f3ecd4166
Martin <martinvicknair@gmail.com>
2023-01-22 16:40:34 -0600

    update readme resources

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 This project uses a two pole switch connected to the GPIO pins of a Raspberry Pi to deactivate the screensaver and change the background picture of a TV Fish Tank. 
 
 ## Resources 
-* [pynput - This library allows you to control and monitor input devices.](https://pynput.readthedocs.io/en/latest/index.html)
+* [pynput - This library allows you to control and monitor input devices](https://pynput.readthedocs.io/en/latest/index.html)
 * [gpiozero - A simple interface to GPIO devices with Raspberry Pi](https://gpiozero.readthedocs.io/en/stable/)
 * [Running scripts on startup](https://learn.sparkfun.com/tutorials/how-to-run-a-raspberry-pi-program-on-startup/all#method-2-autostart)
-* [pcmanfm - Gtk+ based file manager for X Window.](https://www.mankier.com/1/pcmanfm)
+* [pcmanfm - Gtk+ based file manager for X Window](https://www.mankier.com/1/pcmanfm)
 
 #### Author's resources
-* [TvTanks.com website](https://tvtanks.com)
-* [tvtanks-scripts Github Repository](https://github.com/martinvicknair)
+* [Martin Vicknair GitHub](https://github.com/martinvicknair)
+* [tvtanks.com on GitHub](https://github.com/martinvicknair/tvtanks.com)
--- channel_changer.py
+++ channel_changer.py
@@ -4,16 +4,19 @@
 ## -Two pole switch connected via GPIO pins of a Raspberry Pi
 ## -deactivates the screensaver and changes the background picture
 
-import os
-from pynput.mouse import Button, Controller
-from gpiozero import Button
-from signal import pause
+# Import modules
 import glob
 import random
+import os
+from gpiozero import Button
+from pynput.mouse import Controller
+from signal import pause
 from subprocess import call
 
-mouse = Controller()  # Initialize mouse controller 
-button = Button(2)    # Initialize button on GPIO pin 2 
+# Initialize mouse controller 
+mouse = Controller()  
+# Initialize button on GPIO pin 2 
+button = Button(2)    
 
 # Store directory of wallpapers in variable 
 wp_directory="/home/pi/Desktop/backgrounds/*"
@@ -23,16 +26,13 @@ mouse.position = (0, 50)
 
 # Define action to be taken when button is pressed or released 
 def action():  
-
-# Select a random wallpaper from the directory 
-    random_file = random.choice(glob.glob(wp_directory))
-
-# Set the wallpaper using pcmanfm  
-    call(["pcmanfm", "--set-wallpaper=" + random_file])  
-
 # Deactivate screensaver and move mouse away from center of screen 
     os.system("xscreensaver-command -deactivate")  
     mouse.position = (0, 50)  
+# Select a random wallpaper from the directory 
+    random_file = random.choice(glob.glob(wp_directory))
+# Set the wallpaper using pcmanfm  
+    call(["pcmanfm", "--set-wallpaper=" + random_file])  
 
 # Set action to be taken when button is pressed or released 
 button.when_pressed = action  
